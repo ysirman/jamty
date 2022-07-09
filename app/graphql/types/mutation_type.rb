600ab33fae71a33f2a14cc44e0c1efe4d02bd621
ysirman <24975537+ysirman@users.noreply.github.com>
2022-07-09 23:24:43 +0900

    ジャムセッションの中止

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :cancel_jam, mutation: Mutations::CancelJam
     field :create_jam, mutation: Mutations::CreateJam
     field :update_jam, mutation: Mutations::UpdateJam
   end
--- app/models/jam.rb
+++ app/models/jam.rb
@@ -1,3 +1,5 @@
 class Jam < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :user_id, inverse_of: :owned_jams
+
+  scope :canceled, -> { where.not(canceled_at: nil) }
 end
